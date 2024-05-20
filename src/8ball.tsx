6a0821aecf4a566f39bc0b3acf1175fe591e4e41
Valentin Giorgetti <valentin@cohabs.com>
2024-05-20 23:37:23 +0200

    ship 4

--- src/8ball.tsx
+++ src/8ball.tsx
@@ -1,43 +1,35 @@
 import { useState } from "preact/hooks";
-import { useDebounce } from "./hooks/use-debounce";
-import { pickRandomAnswer } from "./lib/magic-8-ball";
+import { use8Ball } from "./hooks/use-8ball";
+import { answers, pickRandomAnswer } from "./lib/magic-8-ball";
 import { cn } from "./utils/cn";
 import { useEventListener } from "./hooks/use-event-listener";
 
 export function Magic8Ball() {
-  const [debouncedAnswer, answer, setAnswer] = useDebounce<string | undefined>(
-    undefined,
-    500
-  );
   const [isAnimating, setIsAnimating] = useState(false);
+  const { text, position, rotation, isReady, roll } = use8Ball(answers);
 
-  const changeAnswer = () => {
-    setIsAnimating(true);
-    setTimeout(() => {
-      setAnswer(pickRandomAnswer());
-      setIsAnimating(false);
-    }, 500);
-  };
-
-  useEventListener(changeAnswer);
+  useEventListener(roll);
 
   return (
     <div
       className={cn(
-        "bg-black text-white rounded-full size-80 aspect-square flex items-center justify-center",
+        "magic bg-black text-white rounded-full size-80 aspect-square flex items-center justify-center",
         {
           "animate-shake-n-zoom": isAnimating,
         }
       )}
     >
-      <div className="bg-white text-black rounded-full size-44 aspect-square overflow-hidden flex items-center justify-center relative">
+      <div className="inner-magic text-black rounded-full size-44 aspect-square overflow-hidden flex items-center justify-center relative">
         <div
           className={cn(
-            "absolute scale triangle flex justify-center w-40 h-32 pt-4 opacity-0 transition-opacity duration-500 ease-in-out bg-[#3148FF] text-white",
-            { "opacity-100": answer == debouncedAnswer && answer != undefined }
+            "absolute scale triangle flex justify-center w-48 h-40 pt-4 opacity-0 transition-opacity duration-500 ease-in-out bg-[#3148FF] text-white",
+            { "opacity-100": isReady }
           )}
+          style={{
+            transform: `rotate(${rotation}deg) translate(${position.offsetX}px, ${position.offsetY}px)`,
+          }}
         >
-          <span className="text-center">{debouncedAnswer}</span>
+          <span className="text-center uppercase [word-spacing:100vw] text-sm">{text}</span>
         </div>
       </div>
     </div>
--- src/hooks/use-8ball.ts
+++ src/hooks/use-8ball.ts
@@ -1,17 +1,49 @@
 import { pickRandomItem } from "../utils/pick-random-item";
+import { useDebounce } from "./use-debounce";
 
 export interface I8BallResponse {
   text: string;
+  position: {
+    offsetX: number;
+    offsetY: number;
+  },
   rotation: number;
 }
 
-export function use8Ball(answers: string[]) {
-  const getResponse = (): I8BallResponse => {
-    return {
-      text: pickRandomItem(answers),
-      rotation: -25 + Math.floor(Math.random() * 50),
-    };
-  };
+const generateBetween = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  return { getResponse };
+export function use8Ball(answers: string[]) {
+  const initState = () => ({
+    text: pickRandomItem(answers),
+    rotation: generateBetween(-25, 25),
+    position: {
+      offsetX: generateBetween(-10, 10),
+      offsetY: generateBetween(-10, 20)
+    }
+  });
+
+  const [debouncedState, state, setState] = useDebounce<I8BallResponse>({
+    text: undefined,
+    rotation: undefined,
+    position: {
+      offsetX: 0,
+      offsetY: 0
+    }
+  }, 500);
+  const { text, rotation, position } = debouncedState;
+  const isReady = state.text === text && state.text !== undefined;
+
+  const reRoll = () => {
+    setState(initState());
+  }
+
+  return {
+    text,
+    position,
+    rotation,
+    isReady,
+    roll: reRoll
+  };
 }
